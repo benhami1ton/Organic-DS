6dfc5e79f5ca8f876319f45012a0575998c9da0f
Ben Hamilton <ben@benhamiltoncreative.com>
2020-11-29 14:01:34 -0600

    Added Full USWDS colors
    Added full spectrum of std USWDS colors in SCSS _colors partial, imported into sketch via PT and created new swatches, begin cleaning up for Stratos connection.

---
+++ test-ds/dist/styledictionary/js/tokens.js
@@ -0,0 +1,7 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 26 Nov 2020 21:02:24 GMT
+ */
+
+export const Primary = "#364bb9";
+export const Blue500 = "#364bb9"; // This is a color that receives the most usage.
