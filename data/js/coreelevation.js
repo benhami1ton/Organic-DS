b2d966d929b6125f83d9f6ef2157110a4184d00f
Ben Hamilton <ben.hamilton@uhsinc.com>
2021-01-05 17:38:57 -0600

    Creating JSON files via Stratos
    Created working Sketch file, Stratos successfully creating JSON objects

---
+++ data/js/coreelevation.js
@@ -0,0 +1,27 @@
+export const coreelevation = {
+  "elevation": {
+    "core": {
+      "12": {
+        "value": "0px 7px 8px -4px rgba(0, 0, 0, 0.2), 0px 12px 17px 2px rgba(0, 0, 0, 0.14), 0px 5px 22px 4px rgba(0, 0, 0, 0.12)"
+      },
+      "24": {
+        "value": "0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12)"
+      },
+      "inner": {
+        "value": "0px 0px 7px 0px inset"
+      },
+      "00": {
+        "value": 0
+      },
+      "02": {
+        "value": "0px 3px 1px -2px rgba(0, 0, 0, 0.12), 0px 2px 2px 0px rgba(0, 0, 0, 0.14),  0px 1px 5px 0px rgba(0, 0, 0, 0.20)"
+      },
+      "04": {
+        "value": "0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)"
+      },
+      "08": {
+        "value": "0px 5px 5px -3px rgba(0, 0, 0, 0.2),  0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)"
+      }
+    }
+  }
+}
---
+++ data/js/coretextlevels.js
@@ -0,0 +1,296 @@
+export const coretextlevels = {
+  "text": {
+    "system": {
+      "Level1": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "4.9"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "3.8"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level2": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "3.9"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "3.1"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level3": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "3.2"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "2.4"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level4": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "2.6"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "2.0"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level5": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "2.1"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "1.6"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level6": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "1.6"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "1.3"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level7": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "1.3"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "1.0"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level8": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "1.1"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "0.8"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level9": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "0.8"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "0.6"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      },
+      "Level10": {
+        "letterSpacing": {
+          "value": "normal"
+        },
+        "textAlign": {
+          "value": "inherit"
+        },
+        "lineHeight": {
+          "value": "0.7"
+        },
+        "marginTop": {
+          "value": "1.0"
+        },
+        "marginBottom": {
+          "value": "1.0"
+        },
+        "color": {
+          "value": "rgba(0,0,0,1)"
+        },
+        "fontFamily": {
+          "value": "NunitoSans-Regular, 'Nunito Sans', sans-serif"
+        },
+        "fontSize": {
+          "value": "0.5"
+        },
+        "fontWeight": {
+          "value": 400
+        }
+      }
+    }
+  }
+}
